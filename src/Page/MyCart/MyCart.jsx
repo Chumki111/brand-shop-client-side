270c240714ef0a7cc192089d900ebcaa3fd2cedd
Chumki khatun <chumkikhatun370@gamil.com>
2023-10-22 11:38:24 +0600

    click add to cart button this data save in database

--- src/Page/MyCart/MyCart.jsx
+++ src/Page/MyCart/MyCart.jsx
@@ -1,9 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+import ViewCart from "./ViewCart";
 
 
 const MyCart = () => {
+    const loadedData = useLoaderData();
+    console.log(loadedData)
     return (
-        <div>
-            myCart
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
+            {
+                loadedData?.map(cart => <ViewCart key={cart._id} cart={cart}></ViewCart>)
+            }
         </div>
     );
 };
